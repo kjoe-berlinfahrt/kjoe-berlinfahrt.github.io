14756015a6ffde5628454129687378e82f1f47fe
Mario Radman <mario.radman@edu.fh-joanneum.at>
2023-01-11 13:06:38 +0100

    try versioning

--- js/use_webworker.js
+++ js/use_webworker.js
@@ -2,6 +2,11 @@
  * USE A WEB WORKER *
  ********************/
 
+function goTo(page) {
+    let versionForURL = version.replace('.', '-');
+    parent.location.href = page + 'html?version=' + versionForURL;
+}
+
 new Worker("/js/webworker.js");
 
 
