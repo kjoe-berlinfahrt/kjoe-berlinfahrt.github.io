2e098938a0968ee7abdc064b84b7f25915670261
Mario Radman <mario.radman@edu.fh-joanneum.at>
2023-01-11 21:47:15 +0100

    make table better responsive; fix linking in conference program; bump version;

--- js/use_webworker.js
+++ js/use_webworker.js
@@ -1,7 +1,7 @@
 /********************
  * USE A WEB WORKER *
  ********************/
-const version = '0.17'
+const version = '0.18'
 
 function goTo(page) {
     let versionForURL = version.replace('.', '-');
