01d11bec61b476b3561ce7c5453223af1a365b65
Mario Radman <mario.radman@edu.fh-joanneum.at>
2023-01-11 22:02:43 +0100

    make table better responsive; bump version;

--- js/use_webworker.js
+++ js/use_webworker.js
@@ -1,11 +1,20 @@
 /********************
  * USE A WEB WORKER *
  ********************/
-const version = '0.19'
+const version = '0.20'
 
 function goTo(page) {
     let versionForURL = version.replace('.', '-');
     parent.location.href = page + '.html?version=' + versionForURL;
+}
+
+function zoomOutMobile() {
+    var viewport = document.querySelector('meta[name="viewport"]');
+
+    if ( viewport ) {
+        viewport.content = "initial-scale=0.1";
+        viewport.content = "width=1200";
+    }
 }
 
 new Worker("/js/webworker.js");
