4ddeae4ddfff6d1b8dc698ecfc25393ad2e1db96
Mario Radman <mario.radman@edu.fh-joanneum.at>
2023-01-11 16:30:25 +0100

    change ul style; bump version as test;

--- js/use_webworker.js
+++ js/use_webworker.js
@@ -1,7 +1,7 @@
 /********************
  * USE A WEB WORKER *
  ********************/
-const version = '0.11'
+const version = '0.12'
 
 function goTo(page) {
     let versionForURL = version.replace('.', '-');
